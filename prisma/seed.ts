f2425f8e502775081f82d943bca90a07fcf53a30
AyushMehta24 <mehtaayush781@gmail.com>
2024-05-31 22:51:19 +0530

    added seeder for garage

---
+++ prisma/seed.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import { garageMasterData } from "../seeders/garageMaster";
+const prisma = new PrismaClient();
+async function main(): Promise<void> {
+  await prisma.garageMaster.createMany({
+    data: garageMasterData,
+  });
+}
+
+main()
+  .catch((e: Error): void => console.log(e))
+  .finally((): void => {
+    prisma.$disconnect();
+  });
